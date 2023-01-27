1dffa6c9d81e34c6d9a6ddf53c1a15cebfa60bbe
Hideki Nakao <80996214+nakaohideki@users.noreply.github.com>
2023-01-26 23:27:21 -0300

    Navigation element creation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { HeaderComponent } from "./components/template/header/header.component"
 
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from './components/template/footer/footer.component'
+import { MatListModule } from "@angular/material/list"
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { FooterComponent } from "./components/template/footer/footer.component"
+import { NavComponent } from "./components/template/nav/nav.component"
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, FooterComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
+	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
