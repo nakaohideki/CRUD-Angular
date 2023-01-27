d2b7ffe551a51d968cf7593d381e3df637131e88
Hideki Nakao <80996214+nakaohideki@users.noreply.github.com>
2023-01-27 20:02:21 -0300

    Table schematic component creation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 import { MatButtonModule } from "@angular/material/button"
 import { MatCardModule } from "@angular/material/card"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
 import { MatListModule } from "@angular/material/list"
+import { MatPaginatorModule } from "@angular/material/paginator"
 import { MatSidenavModule } from "@angular/material/sidenav"
 import { MatSnackBarModule } from "@angular/material/snack-bar"
+import { MatSortModule } from "@angular/material/sort"
+import { MatTableModule } from "@angular/material/table"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { BrowserModule } from "@angular/platform-browser"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { ProductCreateComponent } from "./components/product/product-create/product-create.component"
+import { ProductReadComponent } from "./components/product/product-read/product-read.component"
+import { ProductRead2Component } from "./components/product/product-read2/product-read2.component"
 import { FooterComponent } from "./components/template/footer/footer.component"
 import { HeaderComponent } from "./components/template/header/header.component"
 import { NavComponent } from "./components/template/nav/nav.component"
@@ -32,6 +40,8 @@ import { ProductCrudComponent } from "./views/product-crud/product-crud.componen
 		WhiteDirective,
 		ForDirective,
 		ProductCreateComponent,
+		ProductReadComponent,
+		ProductRead2Component,
 	],
 	imports: [
 		BrowserModule,
@@ -44,6 +54,12 @@ import { ProductCrudComponent } from "./views/product-crud/product-crud.componen
 		MatCardModule,
 		MatSnackBarModule,
 		HttpClientModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
