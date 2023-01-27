e75ed6d395d3061bc3f22b5b7aed5cf904ec7ac2
Hideki Nakao <80996214+nakaohideki@users.noreply.github.com>
2023-01-26 23:54:44 -0300

    Product-crud component creation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { MatSidenavModule } from "@angular/material/sidenav"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { FooterComponent } from "./components/template/footer/footer.component"
 import { NavComponent } from "./components/template/nav/nav.component"
-import { HomeComponent } from "./views/home/home.component"
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from './views/product-crud/product-crud.component'
 
 @NgModule({
 	declarations: [
@@ -21,6 +22,7 @@ import { HomeComponent } from "./views/home/home.component"
 		FooterComponent,
 		NavComponent,
 		HomeComponent,
+  ProductCrudComponent,
 	],
 	imports: [
 		BrowserModule,
